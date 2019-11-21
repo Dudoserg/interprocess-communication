d36e54bc636a39808a120eb2bd493157303de7f4
dudoser <frielender@gmail.com>
2019-11-21 18:45:38 +0700

    Холодос проверяет продукты

--- kruchkova_5.2/Person.h
+++ kruchkova_5.2/Person.h
@@ -1,5 +1,8 @@
-#include "PersonType.h"
 #include <string>
+#include <chrono>
+#include "PersonType.h"
+
+using namespace std::chrono;
 #pragma once
 
 using namespace std;
@@ -17,5 +20,45 @@ public:
 	};
 	Person() {};
 	~Person() {};
+
+	int getRandomNumber(int min, int max)
+	{
+		static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+		// Равномерно распределяем рандомное число в нашем диапазоне
+		return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+	}
+	void printWithDelay(string str, int millis, int TIME_COEFF) {
+		milliseconds time = getTime();
+		int countPoint = 0;
+		while ((getTime() - time) < (milliseconds)millis) {
+			cout << "\r";
+			for (int i = 0; i < str.length() + 4; i++) {
+				cout << " ";
+			}
+			cout << std::flush;
+
+			cout << "\r";
+			cout << str;
+			for (int i = 0; i < countPoint; i++) {
+				cout << ".";
+			}
+			cout << std::flush;
+
+			countPoint++;
+			if (countPoint > 3)
+				countPoint = 0;
+			sleep(0.3 * TIME_COEFF);
+		}
+		cout << endl;
+	}
+	void sleep(int millis) {
+		this_thread::sleep_for(std::chrono::milliseconds(millis));
+	}
+	milliseconds getTime() {
+		milliseconds ms = duration_cast< milliseconds >(
+			system_clock::now().time_since_epoch()
+			);
+		return ms;
+	}
 };
 
